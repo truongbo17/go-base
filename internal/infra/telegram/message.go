31eeb2e5a3076a15ac442873eec11eaf4bf69b5d
truongbo <truongnq@9pay.vn>
2025-01-23 17:00:13 +0700

    telegram

--- internal/infra/telegram/message.go
+++ internal/infra/telegram/message.go
@@ -15,6 +15,33 @@ type MessageStruct struct {
 	Text   string `json:"text"`
 }
 
+type ResponseTelegram struct {
+	Ok     bool           `json:"ok"`
+	Result ResultTelegram `json:"result,omitempty"`
+}
+
+type ResultTelegram struct {
+	MessageID int          `json:"message_id"`
+	From      UserTelegram `json:"from"`
+	Chat      ChatTelegram `json:"chat"`
+	Date      int64        `json:"date"`
+	Text      string       `json:"text"`
+}
+
+type UserTelegram struct {
+	ID        int64  `json:"id"`
+	IsBot     bool   `json:"is_bot"`
+	FirstName string `json:"first_name"`
+	Username  string `json:"username"`
+}
+
+type ChatTelegram struct {
+	ID        int64  `json:"id"`
+	FirstName string `json:"first_name"`
+	Username  string `json:"username"`
+	Type      string `json:"type"`
+}
+
 func SendMessage(chatID string, text string) {
 	logApp := logger.LogrusLogger
 
@@ -25,22 +52,21 @@ func SendMessage(chatID string, text string) {
 		Text:   text,
 	}
 
-	messageBody, err := json.Marshal(message)
-	if err != nil {
-		logApp.Errorf("failed to marshal message: %w", err)
-		return
-	}
-
 	client := httpClient.NewBaseRequest(10 * time.Second)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	headers := map[string]string{
-		"Accept": "application/json",
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
 	}
-	_, _, err = client.Post(ctx, url, headers, messageBody)
+	_, body, err := client.Post(ctx, url, headers, message)
 	if err != nil {
-		logApp.Errorf("Send message to telegram error: %w", err)
+		logApp.Errorln("Send message to telegram error: ", err)
 		return
 	}
+
+	var telegramResponse ResponseTelegram
+	_ = json.Unmarshal(body, &telegramResponse)
+	logApp.Infof("Telegram send message response: %+v", telegramResponse)
 }
